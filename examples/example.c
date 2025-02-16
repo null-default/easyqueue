9b2600f38e5ba19094d5c49ba17c072c0d15d6a3
null-default <>
2025-02-16 14:50:38 -0500

    Updated example program

--- examples/example.c
+++ examples/example.c
@@ -2,13 +2,20 @@
 #include <stdlib.h>
 #include "easyqueue.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+    ezq_status estat = EZQ_STATUS_UNKNOWN;
+    ezq_queue queue = { 0 };
 
-    ezq_init(NULL, 0, NULL, NULL);
+    estat = ezq_init(&queue, NULL, NULL);
 
+#ifndef NDEBUG
     printf("Hello world!\n");
+#endif
+
+    estat = ezq_destroy(&queue, NULL, NULL);
 
     (void)argc;
     (void)argv;
-    return EXIT_SUCCESS;
+    return estat == EZQ_STATUS_SUCCESS ? EXIT_SUCCESS : (int)estat;
 } /* main */
