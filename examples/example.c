c76206ab70908fd74a633b52d1aa1222368621bb
null-default <>
2025-02-16 00:24:29 -0500

    Updated example to use correct parameters

--- examples/example.c
+++ examples/example.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "easyqueue.h"
 
 int main(int argc, char **argv) {
+
+    ezq_init(NULL, 0, NULL, NULL);
+
     printf("Hello world!\n");
 
+    (void)argc;
+    (void)argv;
     return EXIT_SUCCESS;
 } /* main */
